16d9091a09b0e90251a49a840b389f541e803488
Luis Sanchez <luissanchezzamora23@gmail.com>
2023-08-24 18:37:45 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>React + Vite template</h1>
+        <h2>update the dependencies first: npm update</h2>
+       </div>
+    </>
+  )
+}
+
+export default App
